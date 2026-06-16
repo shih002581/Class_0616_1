59a43b8c1bd81fd9d8543eaebceb3fe083a43e33
shih002581 <d11416147@gs.takming.edu.tw>
2026-06-16 16:09:44 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+delegate void MyDel(int a, int b);
+
+public class MyMath
+{
+	public static string str = "多點委派無回傳值";
+	public static void Add(int a, int b)
+	{
+		str += "\n" + (a + b);
+	}
+
+	public static void Sub(int a, int b)
+    {
+		str += "\n" + (a - b);
+    }
+    public static void Mul(int a, int b)
+    {
+        str += "\n" + (a * b);
+    }
+    public static void Div(int a, int b)
+    {
+        str += "\n" + (a / b);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Class_0616_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDel hander = new MyDel(MyMath.Add);
+            hander += new MyDel(MyMath.Sub);
+            hander += new MyDel(MyMath.Mul);
+            hander += new MyDel(MyMath.Div);
+            hander -= new MyDel(MyMath.Add);
+            hander -= new MyDel(MyMath.Sub);
+
+            hander(20,30);
+            Console.WriteLine(MyMath.str);
+
+            MyMath.str = "";
+            hander(15,14);
+            Console.WriteLine(MyMath.str);
+        }
+    }
+}
